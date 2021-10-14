724085623f4fadf923c81376d69ef892825aeee6
root <root@ip-172-31-38-169.us-east-2.compute.internal>
2021-10-14 11:37:04 +0000

    created pom.xml

---
+++ src/main/java/sbi/rest/Api.java
@@ -0,0 +1,11 @@
+package sbi.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author SERPRO
+ */
+@ApplicationPath("api")
+public class Api extends Application {
+}
